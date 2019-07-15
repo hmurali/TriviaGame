812fa7047ad4b32dc80d0e8d30fcfc60751e0875
Hari <augten95@gmail.com>
2019-07-15 10:44:48 -0500

    added trvia properties and questions, options, and answers data

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,4 +13,48 @@ var trivia = {
     timer: 60,
     timerOn: false,
     timerId: '',
+    // questions options and answers data
+    questions: {
+        q1: "Where does the family live on the show?",
+        q2: "Where do the Venturi-McDonald live?",
+        q3: "How many children are in the combined family?",
+        q4: "What is the name of Casey and Derek's high school?",
+        q5: "Who had to move to the basement to give Casey a room upstairs?",
+        q6: "What is the name of Casey's best friend?",
+        q7: "What is the name of Derek's first serious girlfriend?",
+        q8: "What name did Derek give Casey, when she tripped at the stairs in school?",
+        q9: "Who is the youngest child in the combined family?",
+        q10: "What is the name of the boy Casey stars opposite in the school musical, in the episode 'Show-Off Tune'?",
+        q11: "'The Fall'- At the end of this episode, everyone finds out Derek, too, had a nickname once! What is it?",
+        q12: "Where do Edwin and Lizzie always meet when they want to talk, usually about their oldest siblings?"
+    },
+    options: {
+        q1: ["Toronto, Ontario, Canada", "London, Ontario, Canada", "Brampton, Ontario, Canada", "Quebec City, Quebec, Canada"],
+        q2: ["Ontario, Canada", "Quebec, Canada", "Alberta, Canada", "British Columbia, Canada"],
+        q3: ["2", "3", "4", "5"],
+        q4: ["London High", "Toronto High", "Thompson High", "Maple Leaf High"],
+        q5: ["Edwin", "Nora and George", "Derek", "Marti and Lizzie"],
+        q6: ["Emily", "Sam", "Kendra", "Ralph"],
+        q7: ["Emily", "Mary", "Vicki", "Kendra"],
+        q8: ["Fallzilla", "Klutzilla", "Tripzilla", "Clumsy Casey"],
+        q9: ["Edwin", "Lizzie", "Marti", "Casey"],
+        q10: ["Noel", "Joel", "Cole", "Sheldon Schlepper"],
+        q11: ["Derekus", "Dereka", "Dereki", "Derrie"],
+        q12: ["Casey's room", "basement", "Derek's room", "in the game closet"]
+    },
+    answers: {
+        q1: "London, Ontario, Canada",
+        q2: "Ontario, Canada",
+        q3: "5",
+        q4: "Thompson High",
+        q5: "Nora and George",
+        q6: "Emily",
+        q7: "Kendra",
+        q8: "Klutzilla",
+        q9: "Marti",
+        q10: "Noel",
+        q11: "Dereka",
+        q12: "in the game closet"
+    },
+
 }
